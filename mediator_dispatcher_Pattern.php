3346ade90a772a9219badd3f97fbdcd620e5544b
robertke <kel.robert@gmail.com>
2015-05-26 16:15:02 +0300

    Mediator Pattern Example 2

--- mediator_dispatcher_Pattern.php
+++ mediator_dispatcher_Pattern.php
@@ -5,19 +5,79 @@
 
 class InsufficientMoneyAmountException extends Exception {}
 
+class Money
+{
+    private $amount = 0.0;
+
+    private function __construct($amount)
+    {
+        $this->setAmount($amount);
+    }
+
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    public static function create($newAmount)
+    {
+        return new static($newAmount);
+    }
+
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    public function plus($amount)
+    {
+        $this->amount += $amount;
+    }
+
+    public function minus($amount)
+    {
+        $this->amount -= $amount;
+    }
+}
+
+class Rate
+{
+    private $rate;
+
+    private function __construct($rate)
+    {
+        $this->setRate($rate);
+    }
+
+    public function setRate($rate)
+    {
+        $this->rate = $rate;
+    }
+
+    public static function create($newRate)
+    {
+        return new static($newRate);
+    }
+
+    public function getRate()
+    {
+        return $this->rate;
+    }
+}
+
 class Transfer
 {
     private $id;
     private $sender;
     private $receiver;
-    private $amount;
+    private $money;
 
-    public function __construct(Sender $sender, Receiver $receiver, $amount)
+    public function __construct(Sender $sender, Receiver $receiver, Money $money)
     {
         $this->id = uniqid();
         $this->sender = $sender;
         $this->receiver = $receiver;
-        $this->amount = (float)$amount;
+        $this->money = $money;
     }
 
     public function getSender()
@@ -30,18 +90,18 @@ class Transfer
         return $this->receiver;
     }
 
-    public function getAmount()
+    public function getMoney()
     {
-        return $this->amount;
+        return $this->money;
     }
 
     public function withdraw()
     {
-        if($this->getSender()->getDisposableMoney() < $this->amount) {
+        if($this->getSender()->getDisposableMoney()->getAmount() < $this->money->getAmount()) {
             throw new InsufficientMoneyAmountException();
         } else {
-            $this->getSender()->minus($this->amount);
-            $this->getReceiver()->plus($this->amount);
+            $this->sender->minus($this->money->getAmount());
+            $this->receiver->plus($this->money->getAmount());
         }
     }
 }
@@ -49,22 +109,22 @@ class Transfer
 class Sender
 {
     private $name;
-    private $amount = 0.0;
+    private $money;
 
-    public function __construct($name, $amount = null)
+    public function __construct($name, Money $money)
     {
         $this->name = $name;
-        $this->amount = $amount;
+        $this->money = $money;
     }
 
     public function plus($amount)
     {
-        $this->amount += $amount;
+        $this->money->plus($amount);
     }
 
     public function minus($amount)
     {
-        $this->amount -= $amount;
+        $this->money->minus($amount);
     }
 
     public function getName()
@@ -74,29 +134,29 @@ class Sender
 
     public function getDisposableMoney()
     {
-        return $this->amount;
+        return $this->money;
     }
 }
 
 class Receiver
 {
     private $name;
-    private $amount;
+    private $money;
 
-    public function __construct($name, $amount = null)
+    public function __construct($name, Money $money)
     {
         $this->name = $name;
-        $this->amount = $amount;
+        $this->money = $money;
     }
 
     public function plus($amount)
     {
-        $this->amount += $amount;
+        $this->money->plus($amount);
     }
 
     public function minus($amount)
     {
-        $this->amount -= $amount;
+        $this->money->minus($amount);
     }
 
     public function getName()
@@ -106,7 +166,7 @@ class Receiver
 
     public function getDisposableMoney()
     {
-        return $this->amount;
+        return $this->money;
     }
 }
 
@@ -122,6 +182,28 @@ class MoneySendEvent
     public function getTransfer()
     {
         return $this->transfer;
+    }
+}
+
+class ExchangeRateChanged
+{
+    private $rateBefore;
+    private $rateAfter;
+
+    public function __construct(Rate $rateBefore, Rate $rateAfter)
+    {
+        $this->rateBefore = $rateBefore;
+        $this->rateAfter = $rateAfter;
+    }
+
+    public function getRateBefore()
+    {
+        return $this->rateBefore->getRate();
+    }
+
+    public function getRateAfter()
+    {
+        return $this->rateAfter->getRate();
     }
 }
 
@@ -145,10 +227,17 @@ class EventDispatcher
         $this->events[$eventName][] = $array;
     }
 
-    public function notify($eventName)
+    public function notify($eventName, $event = null)
     {
+        if(null !== $event) {
+            $this->addSubscriber($eventName, $event);
+            $event = $this->getSubscriber($eventName);
+        } else {
+            $event = $this->getSubscriber($eventName);
+        }
+
         foreach ($this->events[$eventName] as $item) {
-            call_user_func($item, $this->getSubscriber($eventName));
+            call_user_func($item, $event);
         }
     }
 }
@@ -156,16 +245,26 @@ class EventDispatcher
 class Bank
 {
     private $dispatcher;
+    private $rate;
 
-    public function __construct(EventDispatcher $dispatcher)
+    public function __construct(EventDispatcher $dispatcher, Rate $rate)
     {
         $this->dispatcher = $dispatcher;
+        $this->rate = $rate;
     }
 
     public function transferMoney(Transfer $transfer)
     {
         $transfer->withdraw();
         $this->dispatcher->notify('bank.money_transfer');
+    }
+
+    public function changeRateTo(Rate $rateTo)
+    {
+        $event = new ExchangeRateChanged($this->rate, $rateTo);
+        $this->rate = $rateTo;
+
+        $this->dispatcher->notify('bank.new_exchange_rate', $event);
     }
 }
 
@@ -183,7 +282,10 @@ class StockMarket
 
     public function onMoneyTransfer(MoneySendEvent $event)
     {
-        printf("Stockmarket have been notified about the Transfer (Sender: %s (<strong>%s</strong>) and Receiver: %s (<strong>%s</strong>))", $event->getTransfer()->getSender()->getName(), $event->getTransfer()->getSender()->getDisposableMoney(), $event->getTransfer()->getReceiver()->getName(), $event->getTransfer()->getReceiver()->getDisposableMoney());
+        printf("Stockmarket have been notified about the Transfer (Sender: %s (<strong>%s</strong>) and Receiver: %s (<strong>%s</strong>)) <br>",
+            $event->getTransfer()->getSender()->getName(),
+            $event->getTransfer()->getSender()->getDisposableMoney()->getAmount(), $event->getTransfer()->getReceiver()->getName(),
+            $event->getTransfer()->getReceiver()->getDisposableMoney()->getAmount());
     }
 }
 // AnyInterestedIn wants to know about the money transfers
@@ -195,11 +297,19 @@ class AnyInterestedIn
     {
         $this->dispatcher = $dispatcher;
         $this->dispatcher->addListener('bank.money_transfer', array($this, 'onMoneyTransfer'));
+        $this->dispatcher->addListener('bank.new_exchange_rate', array($this, 'onExchangeRateChanged'));
     }
 
     public function onMoneyTransfer(MoneySendEvent $event)
     {
-        printf("AnyInterestedIn have been notified about the Transfer (Sender: %s and Receiver: %s)", $event->getTransfer()->getSender()->getName(), $event->getTransfer()->getReceiver()->getName());
+        printf("AnyInterestedIn have been notified about the Transfer (Sender: %s and Receiver: %s) <br>",
+            $event->getTransfer()->getSender()->getName(),
+            $event->getTransfer()->getReceiver()->getName());
+    }
+
+    public function onExchangeRateChanged(ExchangeRateChanged $event)
+    {
+        printf("AnyInterestedIn have been notified about the Changed Rate from (%s) to (%s) <br>", $event->getRateBefore(), $event->getRateAfter());
     }
 }
 //////////////////////////////
@@ -211,18 +321,20 @@ $dispatcher = new EventDispatcher();
 $stockMarket = new StockMarket($dispatcher);
 $anyInterestedIn = new AnyInterestedIn($dispatcher);
 
-
-$transfer = new Transfer(new Sender('Jonas', 2000), new Receiver('Petras', 1000), 895.50);
+$transfer = new Transfer(new Sender('Jonas', Money::create(2000)), new Receiver('Petras', Money::create(1000)), Money::create(555.55));
 $dispatcher->addSubscriber('bank.money_transfer', new MoneySendEvent($transfer));
 
 try {
-    $bank = new Bank($dispatcher);
+    $bank = new Bank($dispatcher, Rate::create(2.5));
     $bank->transferMoney($transfer);
     $bank->transferMoney($transfer);
 //    $bank->transferMoney($transfer);
+    $bank->changeRateTo(Rate::create(5));
+    $bank->changeRateTo(Rate::create(9));
+    $bank->changeRateTo(Rate::create(10));
 }
 catch(InsufficientMoneyAmountException $e) {
-    printf("Insufficient Money", $e->getMessage());
+    printf("Insufficient Money Amount", $e->getMessage());
 }
 catch(Exception $e) {
     printf("General", $e->getMessage());
